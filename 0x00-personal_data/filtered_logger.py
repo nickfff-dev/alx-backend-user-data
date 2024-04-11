4b5886e08759dcab323f228922ff564aee49913b
nickfff-dev <hunchographics@gmail.com>
2024-04-12 02:38:34 +0300

    db connector

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -54,7 +54,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connect to a MySQL database using environment variables.
 
     Returns a MySQLConnection object.
