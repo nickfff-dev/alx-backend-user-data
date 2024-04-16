47d72b61f48e840bdf7c5490fbe61c9c02f21435
nickfff-dev <hunchographics@gmail.com>
2024-04-16 23:30:04 +0300

    complete auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -107,3 +107,42 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> User:
+        """
+        Retrieves the User instance for a request.
+
+        Parameters:
+        - request: The request object.
+
+        Returns:
+        - The User instance if the request is authenticated, otherwise None.
+        """
+        if request is None:
+            return None
+
+        # Extract the Authorization header
+        authorization_header = self.authorization_header(request)
+        if authorization_header is None:
+            return None
+
+        # Extract the Base64 part of the Authorization header
+        base64_header = self.extract_base64_authorization_header(
+            authorization_header)
+        if base64_header is None:
+            return None
+
+        # Decode the Base64 encoded string
+        decoded_base64_header = \
+            self.decode_base64_authorization_header(base64_header)
+        if decoded_base64_header is None:
+            return None
+
+        # Extract the user credentials
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_base64_header)
+        if user_email is None or user_pwd is None:
+            return None
+
+        # Retrieve the User instance from the database
+        return self.user_object_from_credentials(user_email, user_pwd)
