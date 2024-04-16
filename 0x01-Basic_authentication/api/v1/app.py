07f1c1361fc4162fe405aea63c1f905c76e02050
nickfff-dev <hunchographics@gmail.com>
2024-04-16 21:43:42 +0300

    validate requests

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,29 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+
+auth_type = getenv('AUTH_TYPE', None)
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """ Filters each request based on the conditions provided
+    """
+    if auth is None:
+        return
+    unauthorized_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, unauthorized_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
