196c3d7c742077b46eff6054d2d7efed84bdb236
nickfff-dev <hunchographics@gmail.com>
2024-04-19 01:31:56 +0300

    check for cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,10 +38,9 @@ def before_request():
 
     if not auth.require_auth(request.path, unauthorized_paths):
         return
-    if auth.authorization_header(
-            request) and auth.session_cookie(request) is None:
-        abort(401)
     if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.session_cookie(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
