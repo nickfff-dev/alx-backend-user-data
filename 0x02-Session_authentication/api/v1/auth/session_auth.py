2ca408e1c5a51f072ad7480c6e83237a74d6e599
nickfff-dev <hunchographics@gmail.com>
2024-04-19 01:11:04 +0300

    inmemory session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """Session authentication module."""
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session authentication class."""
-    pass
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session for a user.
+
+        Args:
+            user_id (str): The user ID for which to create a session.
+
+        Returns:
+            str: The session ID if successful, None otherwise.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
