305e3c12a1dad3b18f7ce5e7e205241e5e56ec0a
nickfff-dev <hunchographics@gmail.com>
2024-04-19 00:19:47 +0300

    me

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,6 +38,9 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
+    if request.current_user is None:
+        abort(403)
 
 
 @app.errorhandler(404)
